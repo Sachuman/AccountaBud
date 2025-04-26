41488528df37612f8b4f249f98e71663d766b663
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2025-04-26 04:18:04 -0700

    add ngrok and ws route

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+import os
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+import requests
+import ngrok
+from fastapi import FastAPI, WebSocket
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global NGROK_PUBLIC_URL
+    listener = await ngrok.forward(8080, authtoken_from_env=True)
+    app.state.ngrok = listener
+    yield
+    listener.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
 
 
 class BrowserUsage(BaseModel):
@@ -11,11 +38,6 @@ class BrowserUsage(BaseModel):
     active_time: int
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, World!"}
-
-
 @app.post("/usage")
 async def usage(usage: BrowserUsage):
     print(f"Received usage data: {usage}")
@@ -23,5 +45,25 @@ async def usage(usage: BrowserUsage):
 
 
 @app.post("/call")
-async def call():
-    return {"message": "Call request received!"}
+def call(phone: str):
+    twiml_endpoint = f"{app.state.ngrok.url()}/twiml"
+    payload = {
+        "Url": twiml_endpoint,
+        "To": phone,
+        "From": TWILIO_PHONE_NUMBER,
+    }
+    resp = requests.post(
+        f"https://api.twilio.com/2010-04-01/Accounts/{ACCOUNT_SID}/Calls.json",
+        data=payload,
+        auth=(ACCOUNT_SID, AUTH_TOKEN),
+    )
+    print(f"Status: {resp.status_code}\n{resp.text}")
+    return {"message": "Call initiated!"}
+
+
+@app.websocket("/")
+async def websocket_endpoint(ws: WebSocket):
+    await ws.accept()
+    while True:
+        data = await ws.receive_bytes()
+        # TODO: Process the call data
